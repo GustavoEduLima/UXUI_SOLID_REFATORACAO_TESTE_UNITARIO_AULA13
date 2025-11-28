aa3d7a46d7b354f41f6027717355b2c563ed27ff
Gustavo Lima <163073778+GustavoEduLima@users.noreply.github.com>
2025-11-28 20:45:07 -0300

    primeira parte

---
+++ calculadora/TesteCalculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        //Teste rapidos 
+        calc.calc(2,3,"+"); //Esperado 5
+        calc.calc(10,4,"-"); //Esperado 6
+        calc.calc(3,5,"*"); //Esperando 15
+        calc.calc(8,2,"/"); //Esperando 4
+        calc.calc(8,0,"/"); //divisão por zero
+        calc.calc(5,5,"x"); //operação inválida
+    }
+}
